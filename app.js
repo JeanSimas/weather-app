cd69e7c3eeb3d5bd37c740f68b5771efa185508d
jeansimas <jeansimas12@hotmail.com>
2019-10-23 17:26:15 -0300

    First Commit

---
+++ app.js
@@ -0,0 +1,12 @@
+// Watch the Call Stack, Callback Queue, and Event Loop class
+
+console.log('Starting')
+setTimeout(() => {
+    console.log('0 seconds')
+}, 0)
+setTimeout(() => {
+    console.log('2 seconds')
+}, 2000)
+
+
+console.log('Stopping')
